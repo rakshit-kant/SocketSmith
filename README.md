9f4d77ed735e9d0d51003f7e9c23bc37b7c777df
rakshit-kant <rakshitkant100@gmail.com>
2026-06-27 15:48:29 +0530

    Tried to do some stuff and made the Half-Completed README

---
+++ README.md
@@ -0,0 +1,25 @@
+# SocketSmith
+
+A very minimal and simple HTTP Server made in C for learning about Networking.
+
+## Features
+
+It just is a Simple HTTP Server. What have you expected??? Those Nerdy Stuff like ICMP, IP and TCP?
+
+## Requirements
+
+- Make
+- Clang/GCC (However I think Clang is better...)
+
+## Installation
+
+```bash
+git clone https://github.com/rakshit-kant/SocketSmith.git
+cd SocketSmith
+
+Do whatever you want I don't know the Project Structure nor have I completely learnt how an HTTP Server works...
+```
+
+## LICENSE
+
+MIT
--- src/main.c
+++ src/main.c
@@ -1,13 +1,35 @@
 #include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 int main() {
-    while (1) {
-        printf("Waiting for Request...\n");
-        sleep(1);
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (server_fd == -1) {
+        perror("Bad Socket\n");
+        return 1;
     }
+
+    struct sockaddr_in {
+        short int sin_family;
+        unsigned short int sin_port;
+        struct in_addr sin_addr;
+        unsigned char sin_zero[8];
+    };
+
+    struct in_addr {
+        uint32_t s_addr;
+    };
+
+    printf("Socket Created!\nFD = %d\n", server_fd);
+
+    // while (1) {
+    //     printf("Waiting for Request...\n");
+    //     sleep(1);
+    // }
     return 0;
 }
